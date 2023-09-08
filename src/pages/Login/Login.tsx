6999268c8cbace5fb6e1f43997bfe05119089938
nguyen hai nguyen <nguyenhainguyen@bincg.com>
2023-09-08 17:05:44 +0700

    commit

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,7 @@
 export default function Login() {
   return (
     <div className='bg-orange-700 '>
-      <div className="bg-[url('../public/imgaes/register-banner.svg')]">
+      <div className="bg-[url('images/register-banner.png')] bg-no-repeat bg-center bg-cover">
         <div></div>
         <div>Form</div>
       </div>
