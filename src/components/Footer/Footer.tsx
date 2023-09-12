8f488d9b76e0ae1ddd2e7428789cff25f7d8a4b7
nguyen hai nguyen <nguyenhainguyen@bincg.com>
2023-09-12 16:57:29 +0700

    commit

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 export default function Footer() {
   return (
     <footer className='bg-slate-950 text-white'>
@@ -8,7 +10,7 @@ export default function Footer() {
             <span className='text-sm'>the quick fox jumps over the lazy dog</span>
           </div>
           <div>
-            <button className='px-10 py-3.5 rounded-md bg-sky-600 font-bold text-sm hover:bg-sky-700'>
+            <button className='px-10 py-3.5 rounded-md bg-indigo-600 font-bold text-sm hover:bg-indigo-500'>
               Contact Us
             </button>
           </div>
@@ -18,27 +20,27 @@ export default function Footer() {
             <h3 className='font-bold text-base mb-5'>Company Info</h3>
             <ul className='font-bold text-sm'>
               <li className='mb-3'>
-                <a href='aaaa' title='About Us'>
+                <Link to='aaaa' title='About Us'>
                   About Us
-                </a>
+                </Link>
               </li>
 
               <li className='mb-3'>
-                <a href='aaaa' title='Carrier'>
+                <Link to='aaaa' title='Carrier'>
                   Carrier
-                </a>
+                </Link>
               </li>
 
               <li className='mb-3'>
-                <a href='aaaa' title='We are hiring'>
+                <Link to='aaaa' title='We are hiring'>
                   We are hiring
-                </a>
+                </Link>
               </li>
 
               <li className='mb-3'>
-                <a href='aaaa' title='Blog'>
+                <Link to='aaaa' title='Blog'>
                   Blog
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
@@ -46,27 +48,27 @@ export default function Footer() {
             <h3 className='font-bold text-base mb-5'>Legal</h3>
             <ul className='font-bold text-sm'>
               <li className='mb-3'>
-                <a href='aaaa' title='About Us'>
+                <Link to='aaaa' title='About Us'>
                   About Us
-                </a>
+                </Link>
               </li>
 
               <li className='mb-3'>
-                <a href='aaaa' title='Carrier'>
+                <Link to='aaaa' title='Carrier'>
                   Carrier
-                </a>
+                </Link>
               </li>
 
               <li className='mb-3'>
-                <a href='aaaa' title='We are hiring'>
+                <Link to='aaaa' title='We are hiring'>
                   We are hiring
-                </a>
+                </Link>
               </li>
 
               <li className='mb-3'>
-                <a href='aaaa' title='Blog'>
+                <Link to='aaaa' title='Blog'>
                   Blog
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
@@ -74,27 +76,27 @@ export default function Footer() {
             <h3 className='font-bold text-base mb-5'>Features</h3>
             <ul className='font-bold text-sm'>
               <li className='mb-3'>
-                <a href='aaaa' title='Business Marketing'>
+                <Link to='aaaa' title='Business Marketing'>
                   Business Marketing
-                </a>
+                </Link>
               </li>
 
               <li className='mb-3'>
-                <a href='aaaa' title='User Analytic'>
+                <Link to='aaaa' title='User Analytic'>
                   User Analytic
-                </a>
+                </Link>
               </li>
 
               <li className='mb-3'>
-                <a href='aaaa' title='Live Chat'>
+                <Link to='aaaa' title='Live Chat'>
                   Live Chat
-                </a>
+                </Link>
               </li>
 
               <li className='mb-3'>
-                <a href='aaaa' title='Unlimited Support'>
+                <Link to='aaaa' title='Unlimited Support'>
                   Unlimited Support
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
@@ -102,27 +104,27 @@ export default function Footer() {
             <h3 className='font-bold text-base mb-5'>Resources</h3>
             <ul className='font-bold text-sm'>
               <li className='mb-3'>
-                <a href='aaaa' title='IOS & Android'>
+                <Link to='aaaa' title='IOS & Android'>
                   IOS & Android
-                </a>
+                </Link>
               </li>
 
               <li className='mb-3'>
-                <a href='aaaa' title='Watch a Demo'>
+                <Link to='aaaa' title='Watch a Demo'>
                   Watch a Demo
-                </a>
+                </Link>
               </li>
 
               <li className='mb-3'>
-                <a href='aaaa' title='Customers'>
+                <Link to='aaaa' title='Customers'>
                   Customers
-                </a>
+                </Link>
               </li>
 
               <li className='mb-3'>
-                <a href='aaaa' title='API'>
+                <Link to='aaaa' title='API'>
                   API
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
@@ -130,26 +132,26 @@ export default function Footer() {
             <h3 className='font-bold text-base mb-5'>Get In Touch</h3>
             <ul className='font-bold text-sm'>
               <li className='mb-3'>
-                <a className='flex gap-2 items-center' href='aaaa' title='(480) 555-0103'>
+                <Link className='flex gap-2 items-center' to='aaaa' title='(480) 555-0103'>
                   <img src='public/images/phone-bold-icon.svg' alt='' /> (480) 555-0103
-                </a>
+                </Link>
               </li>
 
               <li className='mb-3'>
-                <a
+                <Link
                   className='flex gap-2 items-center'
-                  href='aaaa'
+                  to='aaaa'
                   title='4517 Washington Ave. Manchester, Kentucky 39495'
                 >
                   <img src='public/images/location-bold-icon.svg' alt='' /> 4517 Washington Ave. Manchester, Kentucky
                   39495
-                </a>
+                </Link>
               </li>
 
               <li className='mb-3'>
-                <a className='flex gap-2 items-center' href='aaaa' title='debra.holt@example.com'>
+                <Link className='flex gap-2 items-center' to='aaaa' title='debra.holt@example.com'>
                   <img src='public/images/email-bold-icon.svg' alt='' /> debra.holt@example.com
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
@@ -157,15 +159,15 @@ export default function Footer() {
         <div className='py-6 flex justify-between'>
           <div className='font-bold text-sm'>Made With Love By Finland All Right Reserved </div>
           <div className='flex gap-3'>
-            <a href='aaa'>
+            <Link to='aaa'>
               <img src='public/images/fb-blue-icon.svg' alt='' />
-            </a>
-            <a href='aaa'>
+            </Link>
+            <Link to='aaa'>
               <img src='public/images/ins-blue-icon.svg' alt='' />
-            </a>
-            <a href='aaa'>
+            </Link>
+            <Link to='aaa'>
               <img src='public/images/twitter-blue-icon.svg' alt='' />
-            </a>
+            </Link>
           </div>
         </div>
       </div>
--- src/components/RegisterHeader/RegisterHeader.tsx
+++ src/components/RegisterHeader/RegisterHeader.tsx
@@ -1,11 +1,13 @@
+import { Link } from 'react-router-dom'
+
 export default function RegisterHeader() {
   return (
     <header className='xl:container m-auto flex items-center justify-between'>
       <img src='public/images/nhn.png' alt='' />
       <span className='font-bold uppercase text-2xl'>Login</span>
-      <a className='text-blue-600 font-bold text-sm' href='#aa'>
+      <Link className='text-blue-600 font-bold text-sm' to='#aa'>
         Do you need help?
-      </a>
+      </Link>
     </header>
   )
 }
